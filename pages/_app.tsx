545d45c5d97f37e5d75aeda8aabf9991624f7029
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2022-08-23 21:37:53 +0530

    add default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 
 import "tailwindcss/tailwind.css"
 import type { AppProps } from 'next/app'
-
+import '../styles/global.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
