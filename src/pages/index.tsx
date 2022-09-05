8b273df9b078f28a71a0321962c7647359721408
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2022-09-05 20:42:52 +0530

    vote mutation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,17 +8,24 @@ import { inferQueryReponse } from "./api/trpc/[trpc]";
 const btn =
   " text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2";
 const Home: NextPage = () => {
-  const [ids, updateIds] = useState(getOptionsForVote);
+  const [ids, updateIds] = useState(()=>getOptionsForVote());
   const [first, second] = ids;
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
   const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+
+  const voteMutation = trpc.useMutation(["cast-vote"]);
+  const voteForTheBest = (selected: number) => {
+    if(selected === first) {
+      voteMutation.mutate({votedFor:first, votedAgainst:second });
+    } else {
+      voteMutation.mutate({votedFor:second, votedAgainst:first });
+    }
+    updateIds(getOptionsForVote());
+  };
+
   if (firstPokemon.isLoading || secondPokemon.isLoading) {
     return <div>loading...</div>;
   }
-  const voteForTheBest = (selected: number) => {
-    updateIds(getOptionsForVote);
-  };
-
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center"> which pokemon is better ?</div>
@@ -45,9 +52,6 @@ const Home: NextPage = () => {
     </div>
   );
 };
-
-export default Home;
-
 type PokemonFromServer = inferQueryReponse<"get-pokemon-by-id">;
 
 export const PokemonListing: React.FC<{ pokemon: PokemonFromServer,vote:()=>void }> = (
@@ -70,3 +74,7 @@ export const PokemonListing: React.FC<{ pokemon: PokemonFromServer,vote:()=>void
     </div>
   );
 };
+
+
+export default Home;
+
